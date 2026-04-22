696b2ef8d5c10600932e3c45e94bd495354352a4
alumno <alumno@gmail.com>
2026-04-22 19:56:11 +0200

    grivrod: Inicio de la app de flutter

---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.green
+      ) ,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Este es el Home'),
+        ),
+        body: Center(
+          child: FilledButton(
+            onPressed: () { }, 
+            child: Text('Hola mundo!')
+          ),
+        ),
+      ),
+    );
+  }
+}
+ 
