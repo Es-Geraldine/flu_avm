3e27356b000e711d0ac8b27d04576b669968833c
alumno <alumno@gmail.com>
2026-05-04 19:49:19 +0200

    grivrod: impletacion del modo light y dark

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,31 @@
 import 'package:flu_avm/config/config.dart';
+import 'package:flu_avm/presentation/providers/providers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   runApp(
     const ProviderScope(
-      child:MainApp()
-    ));
+      child: MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    
+    final tenebrisModusEst = ref.watch(estTenebrisModusProvider);
+
     return MaterialApp.router(
       routerConfig: appRouter,
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.pink,
-      ),
-    
-      );
+      theme: AppTheme(
+        tenebrisModusEst: tenebrisModusEst,
+        electusColor: Colors.pink,
+      ).getTheme(),
+    );
   }
 }
